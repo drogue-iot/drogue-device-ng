509adb15fae2cb4c9cb2220ed0bffedd8480acbd
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2021-04-28 16:02:35 +0200

    Fix tests

--- device/src/actors/ticker.rs
+++ device/src/actors/ticker.rs
@@ -59,33 +59,3 @@ where
         async move {}
     }
 }
-
-#[cfg(test)]
-mod tests {
-    extern crate std;
-    use super::*;
-    use crate::{testutil::*, *};
-
-    #[derive(Device)]
-    struct TickerDevice {
-        handler: ActorContext<'static, TestHandler>,
-        ticker: ActorContext<'static, Ticker<'static, TestHandler>>,
-    }
-
-    #[drogue::test]
-    async fn test_ticker(mut context: TestContext<TickerDevice>) {
-        let notified = context.signal();
-        context.configure(TickerDevice {
-            handler: ActorContext::new(TestHandler::new(notified)),
-            ticker: ActorContext::new(Ticker::new(Duration::from_secs(1), TestMessage(1))),
-        });
-
-        context.mount(|device| {
-            let handler_addr = device.handler.mount(());
-            (device.ticker.mount(handler_addr), handler_addr)
-        });
-
-        notified.wait_signaled().await;
-        assert_eq!(1, notified.message().unwrap().0);
-    }
-}
